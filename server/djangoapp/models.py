f8e8781b9e42c7dd15520cfc2fac39e61552c110
Aski0407 <nataly.golubitsky@gmail.com>
2025-01-26 13:46:31 -0500

    did car models and some django stuff :)

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,12 +4,6 @@ from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib import admin
-from .models import CarMake, CarModel
-
-# Registering models with their respective admins
-admin.site.register(CarMake)
-admin.site.register(CarModel)
-
 
 # Create your models here.
 
@@ -52,3 +46,4 @@ class CarModel(models.Model):
         ])
     def __str__(self):
         return self.name 
+
