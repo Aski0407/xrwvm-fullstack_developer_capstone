28fd3a4e586ac2c4660f047253c008b57aed50e4
Aski0407 <nataly.golubitsky@gmail.com>
2025-01-27 16:41:45 -0500

    stilll linting omg

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
 from django.shortcuts import render
-#from django.http import HttpResponseRedirect, HttpResponse
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-#from django.shortcuts import get_object_or_404, render, redirect
+# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-#from django.contrib import messages
-#from datetime import datetime
+# from django.contrib import messages
+# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -66,7 +66,7 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-     except Exception as e:
+    except Exception as e:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username)) 
         print(f"Error: {e}")
