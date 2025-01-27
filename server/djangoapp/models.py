781696d01b886f2eaad272355c2d93c52328f71f
Aski0407 <nataly.golubitsky@gmail.com>
2025-01-27 16:35:36 -0500

    still linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,9 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib import admin
+#from django.contrib import admin
 
 # Create your models here.
 
@@ -40,7 +40,8 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
     year = models.IntegerField(
-        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+        default=2023, validators=[MaxValueValidator(2023), 
+        MinValueValidator(2015)]
     )
 
     def __str__(self):
